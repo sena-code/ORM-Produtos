800a86c12baa988521b6bf203a5e061f194e88c8
Vinicius da Silva Sena <61585081+sena-code@users.noreply.github.com>
2020-09-09 18:07:21 -0300

    Add files via upload

---
+++ Senai_Aula6/Senai_Aula6/Interfaces/IPedidoRepository.cs
@@ -0,0 +1,18 @@
+﻿using Senai_Aula6.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai_Aula6.Interfaces
+{
+    interface IPedidoRepository
+    {
+        List<Pedido> Listar();
+        List<Pedido> BuscarPorStatus(string status);
+        Pedido BuscarPorId(Guid id);
+        void Adicionar(Pedido pedido);
+        void Editar(Pedido pedido);
+        void Remover(Guid id);
+    }
+}
---
+++ Senai_Aula6/Senai_Aula6/Interfaces/IProdutoRepository.cs
@@ -0,0 +1,18 @@
+﻿using Senai_Aula6.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai_Aula6.Interfaces
+{
+    interface IProdutoRepository
+    {
+        List<Produto> Listar();
+        List<Produto> BuscarPorNome(string nome);
+        Produto BuscarPorId(Guid id);
+        void Adicionar(Produto produto);
+        void Editar(Produto produto);
+        void Remover(Guid id);
+    }
+}
