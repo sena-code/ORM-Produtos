b1fa2aeb65e9b9931a0d021422369e4a133e51a9
Vinicius da Silva Sena <61585081+sena-code@users.noreply.github.com>
2020-09-13 17:07:04 -0300

    Add files via upload

---
+++ Senai_Aula6/Senai_Aula6/Interfaces/IProdutoItemRepository.cs
@@ -0,0 +1,24 @@
+﻿using Senai_Aula6.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai_Aula6.Interfaces
+{
+    interface IProdutoItemRepository
+    {
+        /// <summary>
+        /// Lista todos os pedidos
+        /// </summary>
+        /// <returns>Retorna uma lista de pedidos</returns>
+        List<Pedido> Listar();
+
+        /// <summary>
+        /// Adiciona um novo pedido
+        /// </summary>
+        /// <param name="pedidosItens">Lista de pedidos itens</param>
+        /// <returns>Objeto Pedido</returns>
+        Pedido Adicionar(List<ProdutoItem> produtoItens);
+    }
+}
